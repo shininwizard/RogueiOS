0410cc14ac9260dd1acd2bc23b0088e65bb82d2a
denis.si <cjvoodoo@gmail.com>
2020-11-04 16:37:36 +0200

    Initial Commit

---
+++ Rogue/RogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogueApp.swift
+//  Rogue
+//
+//  Created by Denis on 04.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct RogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
